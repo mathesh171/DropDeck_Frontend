892746077b8912404f426aa7bd0cc549c3566c54
mathesh171 <mathesh956639@gmail.com>
2025-11-25 16:14:46 +0530

    Logo Changes made. Ready to Use!

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import styles from './Logo.module.css';
+import LogoImg from '../../assets/Logo/Logo-removebg.png';
 
-const Logo = () => {
+const Logo = ({ size = 50 }) => {
   return (
     <div className={styles.logoContainer}>
-      <div className={styles.logoCircle}>
-        <div className={styles.logoText}>
-          <span className={styles.logoInitial}>D</span>
-        </div>
+      <div
+        className={styles.logoCircle}
+        style={{ width: size, height: size }}
+      >
+        <img
+          src={LogoImg}
+          alt="DropDeck logo"
+          className={styles.logoImage}
+        />
       </div>
-      <h1 className={styles.title}>DropDeck</h1>
+
+      <h1 className={styles.wordmark}>
+        <span className={styles.drop}>Drop</span>
+        <span className={styles.deck}>Deck</span>
+      </h1>
     </div>
   );
 };
