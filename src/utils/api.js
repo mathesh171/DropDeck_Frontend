823c73d158ed1ff9e0f2e506055e8e5841968c55
MATHESH M <156519287+mathesh171@users.noreply.github.com>
2025-12-16 21:59:19 +0530

    Fix import path and restore getImageUrl function
    Replaced import path for config.js and restored getImageUrl function.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,12 +1,4 @@
-import { API_LINK } from '../config.js';
+import { API_LINK } from './config.js';
 
-// export const getImageUrl = (path) => `${API_LINK}/uploads/${path}`;
+export const getImageUrl = (path) => `${API_LINK}/uploads/${path}`;
 export const getFileDownloadUrl = (fileId) => `${API_LINK}/api/files/${fileId}/download`;
-
-export const getImageUrl = (filename) => {
-  if (!filename) return null;
-  if (filename.startsWith('http')) return filename;
-  const url = `${API_LINK}/uploads/avatars/${filename}`;
-  console.log('Image URL:', url); 
-  return url;
-}
